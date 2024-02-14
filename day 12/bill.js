f02da78b8e8a9b6a366a277035809b7646de21e1
vedantp0310 <vedantp0310@gmail.com>
2024-02-14 13:47:29 +0530

    secound commit

---
+++ day 12/bill.js
@@ -0,0 +1,10 @@
+let menu = document.getAnimations("menu")
+let breads = document.getElementById("breads")
+let panner = document.getElementById("panner")
+let desert = document.getElementById("desert")
+
+
+menu.addEventListener("focusout",()=>{
+    console.log("hello");
+})
+
--- day11/traffic.js
+++ day11/traffic.js
@@ -6,5 +6,17 @@ let x = setInterval(()=>{
         document.getElementById("red").style.opacity =1
         document.getElementById("green").style.opacity =0
         document.getElementById("counter").innertext = '0' +counter
+    }else if(counter == 10){
+        document.getElementById("red").style.opacity =1
+        document.getElementById("green").style.opacity =0
+        document.getElementById("counter").innertext = counter
+    }else if (counter< 20 && counter > 10){
+        document.getElementById("counter").innerText= secoundarycounter
+        secoundarycounter--
+    }else if(counter == 20){
+        document.getElementById("counter").innerText= secoundarycounter = 10
+        counter = 0
+        secoundarycounter = 10
     }
-})
+    counter++
+},1000)   
